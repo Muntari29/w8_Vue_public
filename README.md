ec5264d41e596fcd9ba2d84f2474d5c24c616af9
Muntari29 <muntrock@gmail.com>
2021-10-10 12:55:51 +0900

    fix: serverless 로직 수정

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## 8주차 과제 배포를 위한 Public 레포
 기존 과제 제출 레포와 같은 형식으로 사용해 주시면 됩니다!
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/d6292c5b-b8bd-4ec3-aeb6-ba17a109f9a0/deploy-status)](https://app.netlify.com/sites/keen-nightingale-20b0b5/deploys)
--- functions/muntariMovie.js
+++ functions/muntariMovie.js
@@ -4,13 +4,13 @@ const { API_END_POINT, API_KEY } = process.env
 
 exports.handler = async function (event) {
     const options = JSON.parse(event.body)
-    const { movieId, title, page=1, body } =options
+    const { movieId, title, page=1, body, method } =options
     
     if (title && !movieId){
         console.log('title!!!')
         const { data } = await axios({
             url: `${API_END_POINT}?apikey=${API_KEY}&s=${title}&page=${page}`,
-            method: 'POST',
+            method,
             headers:{
                 'Content-Type': 'application/json'
             },
@@ -25,7 +25,7 @@ exports.handler = async function (event) {
         console.log('movie!!!!')
         const { data } = await axios({
             url: `${API_END_POINT}?apikey=${API_KEY}&i=${movieId}&plot=full`,
-            method: 'POST',
+            method,
             headers:{
                 'Content-Type': 'application/json'
             },
